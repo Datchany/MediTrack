da4fcee82777fe80cc5f518998dcb5ce7044540b
Datchany <rdatchany530@gmail.com>
2025-07-06 06:49:41 +0530

    🚀 Initial upload of MediTrak-main project to Meditrack repo

--- backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Doctor, Patient, Appointment
-
-admin.site.register(Profile)
-admin.site.register(Doctor)
-admin.site.register(Patient)
-admin.site.register(Appointment)
